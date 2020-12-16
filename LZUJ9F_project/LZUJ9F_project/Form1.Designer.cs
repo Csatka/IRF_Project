08ec9bfa765534d4e4c676c90195a21409ba9372
Csatka <kcsatai@gmail.com>
2020-12-16 19:25:47 +0100

    2

--- LZUJ9F_project/LZUJ9F_project/Form1.Designer.cs
+++ LZUJ9F_project/LZUJ9F_project/Form1.Designer.cs
@@ -29,13 +29,229 @@ namespace LZUJ9F_project
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.winListBox1 = new LZUJ9F_project.WinListBox();
+            this.winListBox2 = new LZUJ9F_project.WinListBox();
+            this.winListBox3 = new LZUJ9F_project.WinListBox();
+            this.winListBox4 = new LZUJ9F_project.WinListBox();
+            this.winListBox5 = new LZUJ9F_project.WinListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numButton1 = new LZUJ9F_project.NumButton();
+            this.numButton2 = new LZUJ9F_project.NumButton();
+            this.numButton3 = new LZUJ9F_project.NumButton();
+            this.numButton4 = new LZUJ9F_project.NumButton();
+            this.numButton5 = new LZUJ9F_project.NumButton();
+            this.SuspendLayout();
+            // 
+            // winListBox1
+            // 
+            this.winListBox1.Enabled = false;
+            this.winListBox1.FormattingEnabled = true;
+            this.winListBox1.Location = new System.Drawing.Point(30, 314);
+            this.winListBox1.Name = "winListBox1";
+            this.winListBox1.Size = new System.Drawing.Size(120, 95);
+            this.winListBox1.TabIndex = 0;
+            this.winListBox1.Visible = false;
+            // 
+            // winListBox2
+            // 
+            this.winListBox2.Enabled = false;
+            this.winListBox2.FormattingEnabled = true;
+            this.winListBox2.Location = new System.Drawing.Point(186, 314);
+            this.winListBox2.Name = "winListBox2";
+            this.winListBox2.Size = new System.Drawing.Size(120, 95);
+            this.winListBox2.TabIndex = 1;
+            this.winListBox2.Visible = false;
+            // 
+            // winListBox3
+            // 
+            this.winListBox3.Enabled = false;
+            this.winListBox3.FormattingEnabled = true;
+            this.winListBox3.Location = new System.Drawing.Point(340, 314);
+            this.winListBox3.Name = "winListBox3";
+            this.winListBox3.Size = new System.Drawing.Size(120, 95);
+            this.winListBox3.TabIndex = 2;
+            this.winListBox3.Visible = false;
+            // 
+            // winListBox4
+            // 
+            this.winListBox4.Enabled = false;
+            this.winListBox4.FormattingEnabled = true;
+            this.winListBox4.Location = new System.Drawing.Point(492, 314);
+            this.winListBox4.Name = "winListBox4";
+            this.winListBox4.Size = new System.Drawing.Size(120, 95);
+            this.winListBox4.TabIndex = 3;
+            this.winListBox4.Visible = false;
+            // 
+            // winListBox5
+            // 
+            this.winListBox5.Enabled = false;
+            this.winListBox5.FormattingEnabled = true;
+            this.winListBox5.Location = new System.Drawing.Point(641, 314);
+            this.winListBox5.Name = "winListBox5";
+            this.winListBox5.Size = new System.Drawing.Size(120, 95);
+            this.winListBox5.TabIndex = 4;
+            this.winListBox5.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(360, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Sorsolás";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "1 találat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(219, 279);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "2 találat";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(368, 279);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "3 találat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(530, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "4 találat";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(675, 279);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "5 találat";
+            // 
+            // numButton1
+            // 
+            this.numButton1.BackColor = System.Drawing.Color.Gold;
+            this.numButton1.Enabled = false;
+            this.numButton1.Location = new System.Drawing.Point(213, 87);
+            this.numButton1.Name = "numButton1";
+            this.numButton1.Size = new System.Drawing.Size(50, 50);
+            this.numButton1.TabIndex = 11;
+            this.numButton1.Text = "numButton1";
+            this.numButton1.UseVisualStyleBackColor = false;
+            // 
+            // numButton2
+            // 
+            this.numButton2.BackColor = System.Drawing.Color.Gold;
+            this.numButton2.Enabled = false;
+            this.numButton2.Location = new System.Drawing.Point(289, 87);
+            this.numButton2.Name = "numButton2";
+            this.numButton2.Size = new System.Drawing.Size(50, 50);
+            this.numButton2.TabIndex = 12;
+            this.numButton2.Text = "numButton2";
+            this.numButton2.UseVisualStyleBackColor = false;
+            // 
+            // numButton3
+            // 
+            this.numButton3.BackColor = System.Drawing.Color.Gold;
+            this.numButton3.Enabled = false;
+            this.numButton3.Location = new System.Drawing.Point(371, 87);
+            this.numButton3.Name = "numButton3";
+            this.numButton3.Size = new System.Drawing.Size(50, 50);
+            this.numButton3.TabIndex = 13;
+            this.numButton3.Text = "numButton3";
+            this.numButton3.UseVisualStyleBackColor = false;
+            // 
+            // numButton4
+            // 
+            this.numButton4.BackColor = System.Drawing.Color.Gold;
+            this.numButton4.Enabled = false;
+            this.numButton4.Location = new System.Drawing.Point(453, 87);
+            this.numButton4.Name = "numButton4";
+            this.numButton4.Size = new System.Drawing.Size(50, 50);
+            this.numButton4.TabIndex = 14;
+            this.numButton4.Text = "numButton4";
+            this.numButton4.UseVisualStyleBackColor = false;
+            // 
+            // numButton5
+            // 
+            this.numButton5.BackColor = System.Drawing.Color.Gold;
+            this.numButton5.Enabled = false;
+            this.numButton5.Location = new System.Drawing.Point(533, 87);
+            this.numButton5.Name = "numButton5";
+            this.numButton5.Size = new System.Drawing.Size(50, 50);
+            this.numButton5.TabIndex = 15;
+            this.numButton5.Text = "numButton5";
+            this.numButton5.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numButton5);
+            this.Controls.Add(this.numButton4);
+            this.Controls.Add(this.numButton3);
+            this.Controls.Add(this.numButton2);
+            this.Controls.Add(this.numButton1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.winListBox5);
+            this.Controls.Add(this.winListBox4);
+            this.Controls.Add(this.winListBox3);
+            this.Controls.Add(this.winListBox2);
+            this.Controls.Add(this.winListBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private WinListBox winListBox1;
+        private WinListBox winListBox2;
+        private WinListBox winListBox3;
+        private WinListBox winListBox4;
+        private WinListBox winListBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private NumButton numButton1;
+        private NumButton numButton2;
+        private NumButton numButton3;
+        private NumButton numButton4;
+        private NumButton numButton5;
     }
 }
 
---
+++ LZUJ9F_project/LZUJ9F_project/WinList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LZUJ9F_project
+{
+    class WinListBox : ListBox
+    {
+        public WinListBox()
+        {
+            Visible = false;
+            ValueMemberChanged += WinListBox_ValueMemberChanged;
+            
+            
+        }
+
+        private void WinListBox_ValueMemberChanged(object sender, EventArgs e)
+        {
+            Visible = true;
+        }
+    }
+}
